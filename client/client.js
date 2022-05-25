3939bbec2f2e0f263b75fe254e4b282fd8ee9985
Bartlomiej Mont <bartlomiejmont@gmail.com>
2022-05-25 18:31:03 +0200

    Add loadTodos

--- client/client.js
+++ client/client.js
@@ -1,3 +1,5 @@
+const todoList = document.getElementById('todoList');
+
 function addTodo() {
     const todoDescription = document.getElementById('todoInput');
     const description = todoDescription.value;
@@ -10,4 +12,29 @@ function addTodo() {
 		},
     })
     .then(response => response.json())
+    loadTodos()
 }
+
+// to samo co funkcja z lini 22
+function resToJson(response){
+    return response.json();
+}
+
+function loadTodos() {
+    fetch('http://localhost:3000/getTodos')
+    .then(response => response.json())
+    // .then(resToJson)
+    .then(renderTodos)
+}
+
+const renderTodos = (todos) => {
+    todoList.innerHTML = '';
+    todos.forEach(t => {
+        todoList.innerHTML += `<li> ${t.description} </li>`
+    })
+}
+
+
+
+
+loadTodos();
